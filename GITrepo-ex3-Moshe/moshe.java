2a523dfe1451897294ee8685591056d417eb412e
moshe <45424025+mmoshem@users.noreply.github.com>
2025-03-30 16:56:41 +0300

    hello

--- GITrepo-ex3-Moshe/moshe.java
+++ GITrepo-ex3-Moshe/moshe.java
@@ -1,7 +1,7 @@
 public class moshe{
     public static void main(String[] args) {
         for (int i=1;i<=10; i++) {
-            System.out.println("Hello from moshe!"+i);
+            System.out.println("Hello "+i);
         }
     }
 }
